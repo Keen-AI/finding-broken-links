dc1bfacc522a20a28587d72fed83a36ed4cbda09
dscience7 <dscience7@users.noreply.github.com>
2021-01-15 17:05:59 +0000

    tidy up code

--- simple_broken_links.py
+++ simple_broken_links.py
@@ -31,67 +31,66 @@ output = StringIO()
 writer = csv.writer(output)
 writer.writerow(['source', 'target'])
 
-with open('link_pairs.txt', 'w') as f:
-    while to_visit:
-        l = to_visit.pop()
-        print(l)
-        url = urljoin(site, l)
+while to_visit:
+    l = to_visit.pop()
+    print(l)
+    url = urljoin(site, l)
 
-        # try:
-        r = requests.get(url)
-        visited[l] = r.status_code
+    # try:
+    r = requests.get(url)
+    visited[l] = r.status_code
 
-        if r.status_code == 200:
-            print(f'visiting: {url}')
-            soup = BeautifulSoup(r.content, 'html5lib')
-            links = [l['href'] for l in soup.find_all('a', href=True)]
-            for link in links:
-                parsed_link = urlparse(link)
-                loc = parsed_link.netloc
-                path = parsed_link.path
-                joined_url = urljoin(site, link)
+    if r.status_code == 200:
+        print(f'visiting: {url}')
+        soup = BeautifulSoup(r.content, 'html5lib')
+        links = [l['href'] for l in soup.find_all('a', href=True)]
+        for link in links:
+            parsed_link = urlparse(link)
+            loc = parsed_link.netloc
+            path = parsed_link.path
+            joined_url = urljoin(site, link)
 
-                if loc == '':
-                    if joined_url not in to_visit and joined_url not in visited.keys() and joined_url[0:4] == 'http':
-                        print(f'adding to internal links: {joined_url}')
-                        to_visit.append(joined_url)
-                        writer.writerow([url, joined_url])
+            if loc == '':
+                if joined_url not in to_visit and joined_url not in visited.keys() and joined_url[0:4] == 'http':
+                    print(f'adding to internal links: {joined_url}')
+                    to_visit.append(joined_url)
+                    writer.writerow([url, joined_url])
 
-                elif loc == base:
-                    if link not in to_visit and link not in visited.keys():
-                        print(f'adding to internal links: {link}')
-                        to_visit.append(link)
-                        writer.writerow([url, link])
+            elif loc == base:
+                if link not in to_visit and link not in visited.keys():
+                    print(f'adding to internal links: {link}')
+                    to_visit.append(link)
+                    writer.writerow([url, link])
 
-                else:
-                    if link not in outlinks and link not in visited.keys():
-                        print(f'adding to external links: {link}')
-                        outlinks.append(link)
-                        writer.writerow([url, link])
+            else:
+                if link not in outlinks and link not in visited.keys():
+                    print(f'adding to external links: {link}')
+                    outlinks.append(link)
+                    writer.writerow([url, link])
 
-    # check the status of external links
-    while outlinks:
-        l = outlinks.pop()
+# check the status of external links
+while outlinks:
+    l = outlinks.pop()
 
-        try:
-            r = requests.get(l)
-            external_visited[l] = r.status_code
+    try:
+        r = requests.get(l)
+        external_visited[l] = r.status_code
 
-        except:
-            external_visited[l] = None
+    except:
+        external_visited[l] = None
 
-    # Create a DataFrame
-    s = pd.Series(visited, name='Response')
-    s.index.name = 'URL'
-    df1 = pd.DataFrame(s)
-    df1['Type'] = 'Internal'
+# Create a DataFrame
+s = pd.Series(visited, name='Response')
+s.index.name = 'URL'
+df1 = pd.DataFrame(s)
+df1['Type'] = 'Internal'
 
-    s = pd.Series(external_visited, name='Response')
-    s.index.name = 'URL'
-    df2 = pd.DataFrame(s)
-    df2['Type'] = 'External'
+s = pd.Series(external_visited, name='Response')
+s.index.name = 'URL'
+df2 = pd.DataFrame(s)
+df2['Type'] = 'External'
 
-    results = pd.concat([df1, df2])
+results = pd.concat([df1, df2])
 
 output.seek(0)
 link_pairs = pd.read_csv(output)
